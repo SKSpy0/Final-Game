6eed24b70578870ec9dda5e5c5a70e64d2edf25f
William Lee <wiklee@ucsc.edu>
2021-05-13 00:13:29 -0700

    wave is working, currently it updates at players location every 2 seconds, will work on making it work with any object and create a group/class specifically for the waves

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -9,23 +9,36 @@ class Player extends Phaser.Physics.Arcade.Sprite{
 
         // Set movement speed
         this.playerVelocity = 200;
+
+        // If player is moving
+        this.moving = false;
     }
 
     update() {
         // Player movement
         if(cursors.left.isDown) {
             this.body.setVelocityX(-this.playerVelocity);
+            this.moving = true;
         } else if (cursors.right.isDown) {
             this.body.setVelocityX(this.playerVelocity);
+            this.moving = true;
         } else {
             this.body.setVelocityX(0);
+            this.moving = false;
         }
         if (cursors.up.isDown) {
             this.body.setVelocityY(-this.playerVelocity);
+            this.moving = true;
         } else if (cursors.down.isDown) {
             this.body.setVelocityY(this.playerVelocity);
+            this.moving = true;
         } else {
             this.body.setVelocityY(0);
+            this.moving = false;
         }
+    }
+
+    isMoving(){
+        return this.moving;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,11 +2,14 @@ class Play extends Phaser.Scene{
     constructor(){
         super("PlayScene");
     }
+
     preload() {
         // Loading temporary assets
         this.load.image('background', './assets/kermitchoke.jpg');
         this.load.image('player', './assets/tempPlayer.png');
+        this.load.image('wave', './assets/wave.png');
     }
+
     create() {
         // Set cursors
         cursors = this.input.keyboard.createCursorKeys();
@@ -18,13 +21,61 @@ class Play extends Phaser.Scene{
         // Add player
         this.player = new Player(this, 1080, 1080, 'player').setOrigin(0.5);
 
+        // Create wave parameters
+        this.wave = this.make.sprite({
+            x: 1080,
+            y: 1080,
+            key: 'wave',
+            add: false
+        });
+        this.newWave = true; // Used for checking when to expand and stop a wave
+
+        //create mask between wave and background
+        this.background.mask = new Phaser.Display.Masks.BitmapMask(this, this.wave);
+
         // Camera setup
         this.cameras.main.setBounds(0, 0, 2160, 2160);
         this.cameras.main.setZoom(0.5);
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setLerp(0.1, 0.1);
+
+        // Wave group
+        this.waveGroup = this.add.group({
+            runChildUpdate:true
+        });
+
+        // Will update and move a wave to player every 2 seconds
+        this.waveCreateClock = this.time.addEvent({
+            delay: 3000,
+            callback: this.moveWave,
+            callbackScope: this,
+            loop: true
+        });
     }
+
+    //moves wave to new spot
+    moveWave(){
+        this.wave.scaleX = 1;
+        this.wave.scaleY = 1;
+        this.wave.x = this.player.x;
+        this.wave.y = this.player.y;
+        this.wave.alpha = 1;
+        this.newWave = true;
+        console.log("wave created");
+    }
+
     update() {
         this.player.update();
+        
+        // Makes wave expand and stop after a certain scale is reached
+        if(this.newWave){
+            this.wave.scaleX += 0.1;
+            this.wave.scaleY += 0.1;
+            if(this.wave.scaleX >= 100){
+                this.newWave = false;
+            }
+        } else {
+            this.wave.alpha = 0;
+        }
     }
 }
