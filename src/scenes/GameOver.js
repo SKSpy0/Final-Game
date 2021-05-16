e96ea1bb87e541b7a4fd548080132a081733a2fa
timothy-tai <titai@ucsc.edu>
2021-05-15 20:52:57 -0700

    added game over

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,29 @@
+class GameOver extends Phaser.Scene{
+    constructor(){
+        super("GameOverScene");
+    }
+
+    create() {
+        this.add.bitmapText(centerWidth, centerHeight/2, 'customFont', 'GAME OVER', 60).setOrigin(0.5);
+        this.return = this.add.bitmapText(centerWidth, centerHeight+100, 'customFont', 'Return to main menu', 28).setOrigin(0.5);
+        // Initialize variables
+        this.clicked = false;
+        this.nextScene = false;
+
+        this.return.on("pointerup", ()=> {
+            this.nextScene = true;
+        });
+
+    }
+
+    update() {
+        // Go to menu scene
+        if (this.nextScene == true && this.clicked == false) {
+            this.cameras.main.fadeOut(1000, 0, 0, 0);
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start('MenuScene');
+            })
+            this.clicked = true;
+        }
+    }
+}
