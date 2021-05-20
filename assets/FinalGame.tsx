8ed19a56ebad4fbe18f8982dacf0198e6186aa6a
William Lee <wiklee@ucsc.edu>
2021-05-19 19:38:27 -0700

    deleted tileset file

--- assets/FinalGame.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="FinalGame" tilewidth="30" tileheight="30" tilecount="324" columns="18">
- <image source="Vignette Escape.png" trans="ff00ff" width="540" height="540"/>
-</tileset>
