79e8049966e9f6464ac00c6c799cc6729a3f0454
Jesse Park <jesapark@ucsc.edu>
2021-05-24 20:14:02 -0700

    Create Tileset.tsx

---
+++ assets/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="TileSet" tilewidth="30" tileheight="30" tilecount="324" columns="18">
+ <image source="VignetteEscapeTileSet.png" trans="ff00ff" width="540" height="540"/>
+</tileset>
