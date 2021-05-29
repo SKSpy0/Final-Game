e1a29d5aa6b801ea76973da16835a45173a0ab39
William Lee <wiklee@ucsc.edu>
2021-05-29 16:44:32 -0700

    fixed that missing texture error on wall.js

--- src/prefabs/Wall.js
+++ src/prefabs/Wall.js
@@ -1,7 +1,7 @@
 // Wall.js is used to create our boundaries of the level and any walls within the level
 class Wall extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture, width, height){
-        super(scene, x, y, texture, width, height);
+        super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.setAlpha(0);
