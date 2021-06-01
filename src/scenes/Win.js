0a04a07161f2cffd75fd9ec5982d445310b7a771
timothy-tai <titai@ucsc.edu>
2021-06-01 15:30:37 -0700

    adjusted text

--- src/scenes/Win.js
+++ src/scenes/Win.js
@@ -7,9 +7,9 @@ class Win extends Phaser.Scene{
     }
     create() {
         this.add.image(0, 0, 'win').setOrigin(0);
-        //this.add.bitmapText(centerWidth, centerHeight/2, 'customFont', 'YOU WIN', 60).setOrigin(0.5);
-        this.return = this.add.bitmapText(centerWidth, centerHeight+100, 'customFont', 'Return to main menu', 36).setOrigin(0.5);
-        this.replay = this.add.bitmapText(centerWidth, centerHeight+50, 'customFont', 'Restart Game', 36).setOrigin(0.5);
+        this.add.bitmapText(centerWidth, centerHeight/3, 'customFont', 'YOU WIN', 60).setOrigin(0.5);
+        this.return = this.add.bitmapText(centerWidth, centerHeight-20, 'customFont', 'Return to main menu', 36).setOrigin(0.5);
+        this.replay = this.add.bitmapText(centerWidth, centerHeight-70, 'customFont', 'Restart Game', 36).setOrigin(0.5);
         this.return.setInteractive({useHandCursor: true});
         this.replay.setInteractive({useHandCursor: true});
         
